6dd3c9a05812e4aef4bd3ec92ca8ef57dd24c2dc
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-08-16 10:45:58 +0900

    Add Searching a Number

---
+++ languages/cpp/practices/basic/searching/searching_a_number.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{ 
+    int t;
+    cin >> t;
+    while (t--) 
+    { 
+        int n, k;
+        cin >> n >> k;
+        int a[n];
+        for (int i = 0; i < n; i++) 
+        { 
+            cin >> a[i];
+        }
+        for (int i = 0; i < n; i++) { 
+            if (a[i] == k) { 
+                cout << i + 1 << endl;
+                break;
+            }
+            if (i + 1 == n) { 
+                cout << -1 << endl;
+            }
+        }
+    }
+
+    return 0;
+}
